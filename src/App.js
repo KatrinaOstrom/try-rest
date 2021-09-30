d7d9e181e17e22b5e4badf6bd80780c86eeadd34
KatrinaOstrom <katrinaostrom@gmail.com>
2021-09-30 14:07:58 -0400

    dad joke added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 
 
-
 const App = () =>{
+    const getJoke = () => {
+        Axios.get("https://icanhazdadjoke.com/"
+        ).then(
+            (response) => {
+                console.log(response);
+            }
+        );
+    };
  return (
- <div className="container">
- <p>mr-headrick</p>
-
- </div>
- );
-}
-export default App;
+<div className="container"><button onClick={getJoke}>Dad Joke</button>
+<br/><p>Hi!</p> 
+        </div>
+        );
+    }
+    export default App;
 
